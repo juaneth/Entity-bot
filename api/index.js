e58ada059f119b016569e8c678132ac30909f6fc
juaneth <euanwalker08@outlook.com>
2021-11-14 19:47:11 +0000

    started building api

---
+++ api/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+app.listen(1455, () => {
+    console.log("Server running on port 1455");
+
+    app.get("/usercount", (req, res, next) => {
+        res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    });
+});
