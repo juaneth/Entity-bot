d329a0ce269f7c1b08ffafbeb0acf6461042f819
juaneth <euanwalker08@outlook.com>
2021-11-14 21:20:56 +0000

    fixed api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,15 @@ app.listen(1455, () => {
     console.log("Server running on port 1455");
 
     app.get("/usercount", (req, res, next) => {
-        res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+        if (req.query.apikey == undefined) {
+            res.status(400);
+            res.send('API Key not defined, you can request one by following the instructions at https://github.com/Flask-Discord/Flask/blob/main/api/README.md');
+        } else {
+            res.status(200);
+            var serverid = req.query.serverid;
+
+            console.log(serverid);
+            res.send(serverid);
+        }
     });
 });
