292e1aa6623ffd959e65d811a48012c43bcb44d0
juaneth <euanwalker08@outlook.com>
2021-11-16 20:39:54 +0000

    made api usable

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const { json } = require("express");
 var express = require("express");
 var app = express();
 app.listen(1455, () => {
-    console.log("Server running on port 1455");
+    console.log("Server running on http://localhost:1455");
+
+
 
     app.get("/usercount", (req, res, next) => {
         if (req.query.apikey == undefined) {
@@ -12,8 +14,29 @@ app.listen(1455, () => {
             res.status(200);
             var serverid = req.query.serverid;
 
-            console.log(serverid);
-            res.send(serverid);
+            console.log("Server ID: " + serverid + " API Key: " + req.query.apikey);
+            res.json({
+                "usercount": "42",
+                "server": "Flask",
+                "server-id": "909232074253295638",
+            });
+        }
+    });
+
+    app.get("/settings", (req, res, next) => {
+        if (req.query.apikey == undefined || "") {
+            res.status(400);
+            res.send('API Key not defined, you can request one by following the instructions at https://github.com/Flask-Discord/Flask/blob/main/api/README.md');
+        } else {
+            res.status(200);
+            var serverid = req.query.serverid;
+
+            console.log("ServerID:" + serverid + " API Key: " + req.query.apikey);
+            res.json({
+                "usercount": "42",
+                "server": "Flask",
+                "server-id": "909232074253295638",
+            });
         }
     });
 });
