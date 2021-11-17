7d2ec62d6d38e4acdb53fa442f1a71439d438eef
juaneth <euanwalker08@outlook.com>
2021-11-17 17:26:36 +0000

    papa

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ const { json } = require("express");
 var express = require("express");
 var app = express();
 app.listen(1455, () => {
-    console.log("Server running on http://localhost:1455");
+    console.log("Server running on http://localhost:1455, test on http://localhost:1455/usercount?apikey=debug");
 
+    app.get("/", (req, res, next) => {
+        res.status(200);
+        console.log('');
+        res.send("Welcome to the Flask API, to get an API key and/or to find the documentation, go to https://github.com/Flask-Discord/Flask/blob/main/api/README.md");
 
+    });
 
     app.get("/usercount", (req, res, next) => {
         if (req.query.apikey == undefined) {
@@ -14,7 +19,7 @@ app.listen(1455, () => {
             res.status(200);
             var serverid = req.query.serverid;
 
-            console.log("Server ID: " + serverid + " API Key: " + req.query.apikey);
+            console.log("Server ID: " + serverid + ", API Key: " + req.query.apikey);
             res.json({
                 "usercount": "42",
                 "server": "Flask",
@@ -24,14 +29,14 @@ app.listen(1455, () => {
     });
 
     app.get("/settings", (req, res, next) => {
-        if (req.query.apikey == undefined || "") {
+        if (req.query.apikey == undefined) {
             res.status(400);
             res.send('API Key not defined, you can request one by following the instructions at https://github.com/Flask-Discord/Flask/blob/main/api/README.md');
         } else {
             res.status(200);
             var serverid = req.query.serverid;
 
-            console.log("ServerID:" + serverid + " API Key: " + req.query.apikey);
+            console.log("Server ID: " + serverid + ", API Key: " + req.query.apikey);
             res.json({
                 "usercount": "42",
                 "server": "Flask",
