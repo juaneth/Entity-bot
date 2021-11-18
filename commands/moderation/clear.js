193f933bd34f0a1965bd435486c235ab2c20deb8
ohmeg <sabbac937@gmail.com>
2021-11-18 20:25:30 +0000

    fixed some stuff

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -34,7 +34,7 @@ module.exports = {
             )
             .setTimestamp();
         message.channel.send({
-            embed: [clearembed]
+            embeds: [clearembed]
         })
     }
 }
