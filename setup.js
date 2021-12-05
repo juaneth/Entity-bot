19eaf4a6a39dd609d30cc1fe1470fe3d4de18e36
Juaneth <euanwalker2008@gmail.com>
2021-12-05 12:58:02 +0000

    touched up the seup js and ping.pong changed

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
 //Run this script using node "node setup.js" to create config files
 
 const fs = require('fs');
+
 try {
     fs.mkdirSync('../flask-config');
     console.log(`Setup made folder: ../flask-config`);
@@ -13,6 +14,7 @@ const defaultconfig = {
     "token": "Place token here!",
     "prefix": "fl."
 }
+
 try {
     fs.writeFileSync("../flask-config/config.json", JSON.stringify(defaultconfig, null, 4), 'utf-8');
     console.log('Writing file finished, open file in ../flask-config/config.json (Next to the base Flask folder inside flask-config) and write in the token that can be found at https://discord.com/developers/applications');
