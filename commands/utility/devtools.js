05feb2097c258050cdf17578c0ba52a40574a6ac
Juaneth <euanwalker2008@gmail.com>
2021-12-06 00:10:48 +0000

    added permission checking to devtools

--- commands/utility/devtools.js
+++ commands/utility/devtools.js
@@ -4,25 +4,52 @@ module.exports = {
     name: "devtools",
     description: "These are the developer tools",
     execute(message, client, args) {
-        if (args[0] === 'on') {
-            let server = message.guild.id;
-            let user = message.author.id;
+        if (message.member.permissions.has('MANAGE_GUILD') || message.member.permissions.has('ADMINISTRATOR')) {
+            if (args[0] === 'on') {
+                let server = message.guild.id;
+                let user = message.author.id;
 
-            const username = client.users.cache.get(user);
+                const username = client.users.cache.get(user);
 
-            console.log("::Devtools on:: SERVER: " + server + " - " + message.guild.name + " || USER: " + user + " - " + message.author.tag);
+                console.log("::Devtools on:: SERVER: " + server + " - " + message.guild.name + " || USER: " + user + " - " + message.author.tag);
 
-            const devtoolsonembed = new Discord.MessageEmbed()
-                .setColor(`RANDOM`)
-                .setTitle(`💻 Developer tools are now on!`)
-                .addFields(
-                    { name: '**User**', value: `${username}`, inline: true }
-                )
-                .setTimestamp();
+                const devtoolsonembed = new Discord.MessageEmbed()
+                    .setColor(`RANDOM`)
+                    .setTitle(`💻 Developer tools are now on!`)
+                    .addFields(
+                        { name: '**For User**', value: `${username}`, inline: true },
+                        { name: '**In Server**', value: `*${message.guild.name}*`, inline: true },
+                        { name: '**Need help?**', value: `Go to https://github.com/Flask-Discord/Flask/blob/main/devtools.MD for more info on how to use them.`, inline: false }
+                    )
+                    .setTimestamp();
 
-            message.channel.send({
-                embeds: [devtoolsonembed]
-            })
+                message.channel.send({
+                    embeds: [devtoolsonembed]
+                })
+            }
+        } else {
+            if (args[0] === 'on') {
+                let server = message.guild.id;
+                let user = message.author.id;
+
+                const username = client.users.cache.get(user);
+
+                console.log("::Devtools tried on - No perms:: SERVER: " + server + " - " + message.guild.name + " || USER: " + user + " - " + message.author.tag);
+
+                const nopermsembed = new Discord.MessageEmbed()
+                    .setColor(`RANDOM`)
+                    .setTitle(`💻 Developer tools need manage server permissions to run.`)
+                    .addFields(
+                        { name: '**For User**', value: `${username}`, inline: true },
+                        { name: '**In Server**', value: `*${message.guild.name}*`, inline: true },
+                        { name: '**Need help?**', value: `Go to https://github.com/Flask-Discord/Flask/blob/main/devtools.MD for more info on how to use them.`, inline: false }
+                    )
+                    .setTimestamp();
+
+                message.channel.send({
+                    embeds: [nopermsembed]
+                })
+            }
         }
     }
 }
